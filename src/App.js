035636d5f50ec0c1d70f96264d28c28a45b3c152
orhanozkercin <orhanozkercin@yahoo.com>
2020-08-02 09:31:44 +0300

    Changed some endpoints of requests

--- src/App.js
+++ src/App.js
@@ -19,10 +19,7 @@ function App() {
 			/>
 			<Row title='Trending Now' fetchUrl={requests.fetchTrending} />
 			<Row title='Animation Movies' fetchUrl={requests.fetchAnimationMovies} />
-			<Row
-				title='Documentary Movies'
-				fetchUrl={requests.fetchDocumentaryMovies}
-			/>
+			<Row title='History Movies' fetchUrl={requests.fetchHistoryMovies} />
 			<Row title='Fantasy Movies' fetchUrl={requests.fetchFantasyMovies} />
 			<Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
 			<Row title='SiFi Movies' fetchUrl={requests.fetchSiFiMovies} />
--- src/requests.jsx
+++ src/requests.jsx
@@ -1,10 +1,10 @@
 const requests = {
 	fetchTrending: `/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`,
-	fetchNetflixOriginals: `/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&sort_by=popularity.desc&page=1&timezone=%20Europe%2FIstanbul&with_networks=213`,
-	fetchDocumentaryMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}}&sort_by=popularity.desc&with_genres=99`,
-	fetchAnimationMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}}&sort_by=popularity.desc&with_genres=16`,
-	fetchFantasyMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&sort_by=popularity.desc&with_genres=14`,
-	fetchRomanceMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}}&sort_by=popularity.desc&with_genres=10749`,
-	fetchSiFiMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}}&sort_by=popularity.desc&with_genres=878`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&page=1&timezone=%20Europe%2FIstanbul&with_networks=213`,
+	fetchHistoryMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=36`,
+	fetchAnimationMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=16`,
+	fetchFantasyMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=14`,
+	fetchRomanceMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=10749`,
+	fetchSiFiMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=878`,
 };
 export default requests;
